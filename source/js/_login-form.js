4d5cb00535554ddee23c75f2db8a0778840a6e5e
Kate Kholyavkina <katerina.kholyavkina@gmail.com>
2016-07-14 15:54:45 +0300

    add empty function _sendForm to login form js

--- source/js/_login-form.js
+++ source/js/_login-form.js
@@ -19,23 +19,40 @@ var loginForm = (function () {
     var
       form = $(this).closest('.form'),
       data = form.serialize();
+      validationParameters = [{
+        id: 'ishuman',
+        type: 'checkbox',
+        checked: true,
+        errorMsg: 'Роботам здесь не место'
+      }, {
+        id: 'notrobot-yes',
+        type: 'radio',
+        checked: true,
+        errorMsg: 'Роботам здесь не место'
+      }, {
+        id: 'notrobot-no',
+        type: 'radio',
+        checked: false,
+        errorMsg: 'Роботам здесь не место'
+      }];
 
-    validation.validateForm(form, [{
-      id: 'ishuman',
-      type: 'checkbox',
-      checked: true,
-      errorMsg: 'Роботам здесь не место'
-    }, {
-      id: 'notrobot-yes',
-      type: 'radio',
-      checked: true,
-      errorMsg: 'Роботам здесь не место'
-    }, {
-      id: 'notrobot-no',
-      type: 'radio',
-      checked: false,
-      errorMsg: 'Роботам здесь не место'
-    }]);
+    if (validation.validateForm(form, validationParameters)) {
+      _sendForm(form);
+    }
+
+    function _sendForm(form){
+      // $.ajax({
+      //   type: "POST",
+      //   url: 'assets/php/mail.php',
+      //   cache: false,
+      //   data: form.serialize()
+      // }).done(function(html){
+      //   modal.showMessage(html);
+      // }).fail(function(html){
+      //   modal.showMessage('Сообщение не отправлено!');
+      // })
+    };
+
 
   }
 
