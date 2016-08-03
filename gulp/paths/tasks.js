e192fc589539e0aab61714c3d1d4d4e62b9b39ee
Kate Kholyavkina <katerina.kholyavkina@gmail.com>
2016-08-03 18:48:21 +0300

    fix gulp paths

--- gulp/paths/tasks.js
+++ gulp/paths/tasks.js
@@ -12,6 +12,13 @@ module.exports = [
   './gulp/tasks/js.lint.js',
   './gulp/tasks/copy.image.js',
   './gulp/tasks/copy.fonts.js',
-  './gulp/tasks/copy.php.js'
+  './gulp/tasks/copy.php.js',
+  './gulp/tasks/public.sass.js',
+  './gulp/tasks/public.copy.jade.js',
+  './gulp/tasks/public.js.foundation.js',
+  './gulp/tasks/public.js.process.js',
+  './gulp/tasks/public.copy.image.js',
+  './gulp/tasks/public.copy.fonts.js',
+  './gulp/tasks/public.css.foundation.js'
 ];
 
