1d908ac3894c571e9d529c88bccc41e649b25d71
Kate Kholyavkina <katerina.kholyavkina@gmail.com>
2016-08-03 21:12:47 +0300

    refactoring of  middleware.js

--- routes/admin/middleware.js
+++ routes/admin/middleware.js
@@ -1,10 +1,7 @@
 module.exports = (req, res, next) => {
-  console.log('middleware.js req.session.isAdmin:', req.session.isAdmin);
   if (!req.session.isAdmin) {
-    console.log('middleware.js redirect');
     res.redirect('/');
   } else {
-    console.log('middleware.js next()');
     next();
   }
 };
