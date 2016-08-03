e032c892f01df8f0c41f176cef17a690c9bab3fa
Kate Kholyavkina <katerina.kholyavkina@gmail.com>
2016-08-04 00:25:57 +0300

    finally fixed gulpfile to copy jade templates

--- gulp/tasks/public.copy.jade.js
+++ gulp/tasks/public.copy.jade.js
@@ -1,27 +1,8 @@
 'use strict';
 
-// module.exports = function() {
-//   $.gulp.task('jade', function() {
-//     var YOUR_LOCALS = './content.json';
-//     // console.log(JSON.parse($.fs.readFileSync(YOUR_LOCALS, 'utf-8')));
-//     return $.gulp.src($.path.template)
-//       .pipe($.gp.jade({ 
-//         locals: JSON.parse($.fs.readFileSync(YOUR_LOCALS, 'utf-8')),
-//         pretty: true 
-//       }))
-//       .on('error', $.gp.notify.onError(function(error) {
-//         return {
-//           title: 'Jade',
-//           message:  error.message
-//         }
-//        }))
-//       .pipe($.gulp.dest($.config.root));
-//   });
-// };
-
 module.exports = function() {
   $.gulp.task('public.copy.jade', function() {
-    return $.gulp.src($.path.template)
+    return $.gulp.src('./source/template/**/*.*'  , { since: $.gulp.lastRun('public.copy.jade') })
       .pipe($.gulp.dest($.config.public + '/markups/_pages'));
   });
 };
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,11 +2,11 @@
 
 module.exports = function() {
   $.gulp.task('watch', function() {
-    $.gulp.watch('./source/js/**/*.js', $.gulp.series('js.process'));
-    $.gulp.watch('./source/style/**/*.scss', $.gulp.series('sass'));
-    $.gulp.watch('./source/template/**/*.jade', $.gulp.series('jade'));
-    $.gulp.watch('./source/images/**/*.*', $.gulp.series('copy.image'));
-    $.gulp.watch('./source/fonts/**/*.*', $.gulp.series('copy.fonts'));
+    $.gulp.watch('./source/js/**/*.js', $.gulp.series('js.process', 'public.js.process'));
+    $.gulp.watch('./source/style/**/*.scss', $.gulp.series('sass', 'public.sass'));
+    $.gulp.watch('./source/template/**/*.jade', $.gulp.series('jade', 'public.copy.jade'));
+    $.gulp.watch('./source/images/**/*.*', $.gulp.series('copy.image', 'public.copy.image'));
+    $.gulp.watch('./source/fonts/**/*.*', $.gulp.series('copy.fonts', 'public.copy.fonts'));
     $.gulp.watch('./source/php/**/*.*', $.gulp.series('copy.php'));
   });
 };
